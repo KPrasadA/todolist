bb53fdec9f20062fc2691296f76634ff964bf7bc
KPrasadA <krishnaprasad9798@gmail.com>
2023-05-01 16:28:31 +0530

    V1.1 Code done till checkbox status

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{useState} from 'react'
+import './App.css'
 
 function App() {
+    const [ToDos, setToDos] = useState([]);
+    const [todo , setTodo] = useState('');
+
+    function inputHandler(event){
+        setTodo(event.target.value);
+    }
+    function addListHandler(){
+        setToDos([...ToDos ,{id:Date.now(), text:todo, status: false}]);
+    }
+    function enterHandler(event){
+        if(event.code ==='Enter')
+        {
+          setToDos([...ToDos ,{id:Date.now(), text:todo, status: false}]);
+        }
+    }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+        <div className='mainHeading'>
+         <h1>ToDo List</h1> 
+        </div>
+        <div className='subHeading'>
+          <h2>Create your ToDo List</h2>
+        </div>
+        <div className='input'>
+          <input onChange={inputHandler} onKeyDown={enterHandler} type="text" placeholder='🖊 Add items...' />
+            <i onClick={addListHandler}  className="fas fa-plus"></i>
+        </div>
+                 
+          <div className='todos' >
+          {
+            ToDos.map((data)=>{
+              return(
+              <div className='todo todoAnimationSelected'>
+                <div className="left">
+                  <input onChange={(event)=>{
+                    console.log(event.target.checked);
+                    console.log(data);
+                    setToDos(ToDos.filter(newObj=>{
+                      if(newObj.id === data.id){
+                        newObj.status = event.target.checked
+                      }
+                      return newObj
+                    }))
+                  }} data={data.status} type="checkbox" />
+                  <p>{data.text}</p>
+                </div>
+                <div className="right">
+                  <i className='fas fa-time'></i>
+                </div>
+              </div>
+              )
+             
+            })
+          }
+        </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
