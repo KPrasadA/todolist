efce0588dfbfbb0699618d0e31aaf0250c443f26
KPrasadA <krishnaprasad9798@gmail.com>
2023-05-01 18:20:23 +0530

    added delete and editing options to todoList

--- src/App.js
+++ src/App.js
@@ -1,65 +1,120 @@
 import React ,{useState} from 'react'
 import './App.css'
 
-function App() {
-    const [ToDos, setToDos] = useState([]);
-    const [todo , setTodo] = useState('');
 
-    function inputHandler(event){
-        setTodo(event.target.value);
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [todoText, setTodoText] = useState('');
+  const [editMode, setEditMode] = useState(false);
+  const [editTodoId, setEditTodoId] = useState(null);
+  const [editTodoText, setEditTodoText] = useState('');
+
+  function handleInputChange(event) {
+    setTodoText(event.target.value);
+  }
+
+  function handleAddTodo() {
+    setTodos([...todos, { id: Date.now(), text: todoText, completed: false }]);
+    setTodoText('');
+  }
+
+  function handleKeyDown(event) {
+    if (event.code === 'Enter') {
+      handleAddTodo();
     }
-    function addListHandler(){
-        setToDos([...ToDos ,{id:Date.now(), text:todo, status: false}]);
-    }
-    function enterHandler(event){
-        if(event.code ==='Enter')
-        {
-          setToDos([...ToDos ,{id:Date.now(), text:todo, status: false}]);
-        }
-    }
-  
+  }
+
+  function handleEdit(id) {
+    const todo = todos.find(todo => todo.id === id);
+    setEditTodoId(id);
+    setEditTodoText(todo.text);
+    setEditMode(true);
+  }
+
+  function handleEditInputChange(event) {
+    setEditTodoText(event.target.value);
+  }
+
+  function handleSaveEdit() {
+    setTodos(todos.map(todo => {
+      if (todo.id === editTodoId) {
+        return { ...todo, text: editTodoText };
+      } else {
+        return todo;
+      }
+    }));
+    setEditMode(false);
+    setEditTodoId(null);
+    setEditTodoText('');
+  }
+
+  function handleCancelEdit() {
+    setEditMode(false);
+    setEditTodoId(null);
+    setEditTodoText('');
+  }
+
+  function handleToggleComplete(id) {
+    setTodos(todos.map(todo => {
+      if (todo.id === id) {
+        return { ...todo, completed: !todo.completed };
+      } else {
+        return todo;
+      }
+    }));
+  }
+
+  function handleDelete(id) {
+    setTodos(todos.filter(todo => todo.id !== id));
+  }
+
   return (
     <div className='app'>
-        <div className='mainHeading'>
-         <h1>ToDo List</h1> 
-        </div>
-        <div className='subHeading'>
-          <h2>Create your ToDo List</h2>
-        </div>
-        <div className='input'>
-          <input onChange={inputHandler} onKeyDown={enterHandler} type="text" placeholder='🖊 Add items...' />
-            <i onClick={addListHandler}  className="fas fa-plus"></i>
-        </div>
-                 
-          <div className='todos' >
-          {
-            ToDos.map((data)=>{
-              return(
-              <div className='todo todoAnimationSelected'>
-                <div className="left">
-                  <input onChange={(event)=>{
-                    console.log(event.target.checked);
-                    console.log(data);
-                    setToDos(ToDos.filter(newObj=>{
-                      if(newObj.id === data.id){
-                        newObj.status = event.target.checked
-                      }
-                      return newObj
-                    }))
-                  }} data={data.status} type="checkbox" />
-                  <p>{data.text}</p>
+      <div className='mainHeading'>
+        <h1>ToDo List</h1>
+      </div>
+      <div className='subHeading'>
+        <h2>Create your ToDo List</h2>
+      </div>
+      <div className='input'>
+        <input
+          type='text'
+          placeholder='🖊 Add items...'
+          value={todoText}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
+        />
+        <button onClick={handleAddTodo}><i className='fas fa-plus'></i></button>
+      </div>
+
+      <div className='todos'>
+        {todos.map(todo => (
+          <div className={`todo ${todo.completed ? 'completed' : ''}`} key={todo.id}>
+            {editMode && editTodoId === todo.id ?
+              <>
+                <input type='text' value={editTodoText} onChange={handleEditInputChange} />
+                <button onClick={handleSaveEdit}><i className='fas fa-check'></i></button>
+                <button onClick={handleCancelEdit}><i className='fas fa-times'></i></button>
+              </>
+              :
+              <>
+                <div className='left'>
+                  <input type='checkbox' checked={todo.completed} onChange={() => handleToggleComplete(todo.id)} />
+                  <p>{todo.text}</p>
                 </div>
-                <div className="right">
-                  <i className='fas fa-time'></i>
+                <div className='right'>
+                  <button onClick={() => handleEdit(todo.id)}><i className='fas fa-pen'></i></button>
+                  <button onClick={() => handleDelete(todo.id)}><i className='fas fa-trash'></i></button>
                 </div>
-              </div>
-              )
-             
-            })
-          }
-        </div>
+              </>
+            }
+          </div>
+        ))}
+      </div>
     </div>
   )
-}
-
+  }
 export default App
+  
+  
+ 
